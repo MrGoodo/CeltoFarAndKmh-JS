5c91c70dbbd16b6b7c280d983c18e495fa65b9bc
Nick Kandelaki <goodo.nick@gmail.com>
2022-09-20 15:17:34 +0300

    CeltoFarAndKmh-JS

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 // Средняя скорость //
 
 function sum(a, b, c) {
-    return (a + b + c) / 3;
-  }
-  sum(5, 24, 20)
-
-
-  // Фаренгейт в Цельсий//
-  function Far(cTemp) {
-    return (cTemp * 1.8) + 32;
-  }
-  Far (3)
-
-  // Цельсий в Фаренгейт//
-  function Cel(fTemp) {
-    return (fTemp - 32) * (5/9);
-  }
-  Cel (3)
-
-  // Вопрос и ответ (if else) //
-
-  let answer = prompt ('Авторизуйтесь с помощью Логина');
-  if (answer == 'Nika') {
-     alert ('Вы прошли Авторизацию!');
+  return (a + b + c) / 3;
 }
-else { 
-  alert('Вы не прошли авторизацию!');
+sum(5, 24, 20);
+
+// Фаренгейт в Цельсий//
+function Far(cTemp) {
+  return cTemp * 1.8 + 32;
+}
+Far(3);
+
+// Цельсий в Фаренгейт//
+function Cel(fTemp) {
+  return (fTemp - 32) * (5 / 9);
+}
+Cel(3);
+
+// Вопрос и ответ (if else) //
+
+let answer = prompt("Авторизуйтесь с помощью Логина");
+if (answer == "Nika") {
+  alert("Вы прошли Авторизацию!");
+} else {
+  alert("Вы не прошли авторизацию!");
 }
